c1f3e0c517c682c22ccd623375e7161a1f7d2410
Atsushi Nagase <a@ngs.io>
2017-04-15 00:44:08 +0900

    Replace invalid byte sequence in UTF-8

--- lib/middleman-blog-similar/tagger/mecab.rb
+++ lib/middleman-blog-similar/tagger/mecab.rb
@@ -7,6 +7,8 @@ module Middleman
         class Mecab
           def call(article)
             Natto::MeCab.new.parse(article.body.gsub(%r{</?[^>]+>}, ''))
+                        .encode('UTF-16BE', 'UTF-8', invalid: :replace, undef: :replace, replace: '')
+                        .encode('UTF-8')
                         .split("\n")
                         .map { |l| l.split("\t") }
                         .select { |l| l[1] && l[1].start_with?('名詞,一般') }
